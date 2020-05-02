0d7a04d482c2742efe37e5dadefeb462d5747981
JayLim <ddx180874@gmail.com>
2020-05-02 18:04:56 +0400

    [REFACTORING] added "native-base", merged with stateful_component

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,24 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import {HELLO_WORLD} from "../configuration/Constants";
 import StatefulComponent from "./StatefulComponent";
+import {
+    Container, Header, Title,
+    Content, Footer, FooterTab,
+    Button, Left, Right,
+    Body, Icon, Text
+} from 'native-base';
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+const textContentStyles = {
+    marginTop:25,
+    marginLeft:15,
+    marginBottom:0,
+    marginRight:15
+}
 
 const HelloWorld = () => {
     return (
-        <View style={styles.container}>
-            <Text>TEST NEW COMPONENT</Text>
-            <Text>{HELLO_WORLD}</Text>
-            <StatefulComponent/>
-        </View>
+        <Container>
+            <Header>
+                <Left>
+                    <Button transparent>
+                        <Icon name='menu' />
+                    </Button>
+                </Left>
+                <Body>
+                    <Title>Login</Title>
+                </Body>
+                <Right />
+            </Header>
+            <Content>
+                <Text style={textContentStyles}>
+                    TEST NEW COMPONENT
+                </Text>
+                <Text style={textContentStyles}>
+                    {HELLO_WORLD}
+                </Text>
+                <StatefulComponent/>
+            </Content>
+            <Footer>
+                <FooterTab>
+                    <Button full>
+                        <Text>Footer</Text>
+                    </Button>
+                </FooterTab>
+            </Footer>
+        </Container>
     );
 }
 
--- src/components/StatefulComponent.tsx
+++ src/components/StatefulComponent.tsx
@@ -1,16 +1,31 @@
 import React, {Component} from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {CHANGE_BUTTON_TEXT} from "../configuration/Constants";
+import {Text, Button} from 'native-base';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 
 class StatefulComponent extends Component<any, any> {
 
-    constructor(props) {
+    constructor({props}: { props: any }) {
         super(props);
         this.state = {
             message: "Unstable Message ",
-            counter: 0
+            counter: 0,
+            loading: true
         }
         this.onPress = this.onPress.bind(this);
+    }
+
+    async componentDidMount() {
+        await Font.loadAsync({
+            'Roboto': require('native-base/Fonts/Roboto.ttf'),
+            'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+            ...Ionicons.font,
+        })
+        this.setState({
+            loading: false
+        })
     }
 
     onPress() {
@@ -23,6 +38,10 @@ class StatefulComponent extends Component<any, any> {
     }
 
     render() {
+        if(this.state.loading) {
+            return <View/>;
+        }
+
         const {message} = this.state;
 
         const styles = StyleSheet.create({
@@ -31,13 +50,16 @@ class StatefulComponent extends Component<any, any> {
                 backgroundColor: '#fff',
                 alignItems: 'center',
                 justifyContent: 'center',
+                marginTop: "50%"
             },
         });
 
         return (
             <View style={styles.container}>
                 <Text>{message}</Text>
-                <Button title={CHANGE_BUTTON_TEXT} onPress={this.onPress}/>
+                <Button primary onPress={this.onPress}>
+                    <Text>{CHANGE_BUTTON_TEXT}</Text>
+                </Button>
             </View>
         )
     }
